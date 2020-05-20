e20368797ec230d1f05ab4f64816ad14fba4f42c
Paulo Bautista <paulosbautista@gmail.com>
2020-05-20 12:06:18 +0800

    updated logic

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ def main():
     df = pd.read_csv('sample_route_data.csv')
 
     df['result'] = df.apply(get_scheme_rate,axis=1)
+    df['incentive_pay'] = df.apply(lambda row: row.result[0] * row.parcels_delivered,axis=1)
     df['delivery_pay']  = [x[0] for x in df.result]
 
-    df.groupby(['rider_id','rider_name','hub_id','hub_name','driver_type']).agg({'parcels_delivered':'sum','route_date':'nunique','delivery_pay':'sum'}).reset_index().to_csv('rider_level.csv',index=False)
+    df.groupby(['rider_id','rider_name','hub_id','hub_name','driver_type']).agg({'parcels_delivered':'sum','route_date':'nunique','delivery_pay':'sum','incentive_pay':'sum'}).reset_index().to_csv('rider_level.csv',index=False)
     df.groupby(['hub_id','hub_name']).agg({'parcels_delivered':'sum','route_date':'nunique','delivery_pay':'sum'}).reset_index().to_csv('hub_level.csv',index=False)
 
+    print("results generated!")
+    
 if __name__ == '__main__':
     main()
 
---
+++ readME.md
@@ -0,0 +1,13 @@
+# This is to compute for payroll
+
+This uses payroll data and payscheme matrix to output computation
+
+## Getting Started
+
+Route Data - query from metabase:
+`select route_id,  route_date, courier_id as rider_id, courier_name as rider_name , depot_id as hub_id , depot_name as hub_name, scheme_type, courier_type as driver_type, parcels_delivered   from ph_views.fleet_performance_base_data where depot_id in (1,3) and fleet_type = 'Last - Mile' and route_start between {{start_date}} and {{end_date}}`
+
+Pay Scheme Data -
+csv_template = `https://docs.google.com/spreadsheets/d/1xlSAU5Idg-CgXS6Z7iwYS6AQlOaa2CGlo7ZJcpMGCMY/edit?usp=sharing`
+
+columns = (hub_id,hub_name,zone_id,zone_name,driver_type,min_parcel,max_parcel,daily_min_wage,	per_parcel_rate,fixed_component,fixed_incentive_component)
